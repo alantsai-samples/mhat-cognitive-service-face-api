0b1c367913980ebb4c81ac0f4ccb9259f69ba21d
Alan Tsai <alan@alantsai.net>
2018-09-07 07:51:26 +0800

    準備好呼叫Face Api的detect

--- src/csharp/MHAT.Face.Api.SimpleConsoleApp/MHAT.Face.Api.SimpleConsoleApp/Program.cs
+++ src/csharp/MHAT.Face.Api.SimpleConsoleApp/MHAT.Face.Api.SimpleConsoleApp/Program.cs
@@ -9,12 +9,18 @@ namespace MHAT.Face.Api.SimpleConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var subscriptionKey = "";
             var enpointUrl = "https://eastasia.api.cognitive.microsoft.com/face/v1.0";
 
+            var imageUrl = "https://raw.githubusercontent.com/Microsoft/Cognitive-Face-Windows/master/Data/detection1.jpg";
+
+            var faceAttr = new[] { FaceAttributeType.Age, FaceAttributeType.Gender, FaceAttributeType.Emotion };
+
             var client = new FaceServiceClient(subscriptionKey, enpointUrl);
+
+            var faces = await client.DetectAsync(imageUrl, returnFaceAttributes: faceAttr);
         }
     }
 }
